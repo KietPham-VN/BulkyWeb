4fce7ba0c72ff17ce1d3b69621012b62abf3948c
Phạm Anh Kiệt <phamanhkiet.dev@gmail.com>
2024-11-02 13:37:03 +0700

    more feature incoming

--- BulkyDataAccess/Repository/CategoryRepository.cs
+++ BulkyDataAccess/Repository/CategoryRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Bulky.DataAccess.Data;
+﻿using Bulky.DataAccess.Data;
 using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
 
@@ -20,5 +15,6 @@ namespace Bulky.DataAccess.Repository {
         public void Update(Category obj) {
             _db.Categories.Update(obj);
         }
+
     }
 }
---
+++ BulkyWeb/wwwroot/js/product.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    $('#tblData').DataTable({
+        "ajax": {
+            "url": "/admin/product/getall",
+        },
+        "columns": [
+            { data: 'title', "width": "25%" },
+            { data: 'isbn', "width": "15%" },
+            { data: 'listPrice', "width": "15%" },
+            { data: 'author', "width": "15%" },
+            { data: 'category.name', "width": "10%" }
+
+        ]
+    });
+}

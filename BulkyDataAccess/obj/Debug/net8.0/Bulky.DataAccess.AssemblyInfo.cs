7dd4753f6dc3fc3d3db471eb485de84bd8f851d4
Phạm Anh Kiệt <phamanhkiet.dev@gmail.com>
2024-11-04 00:04:49 +0700

    add home page

--- BulkyDataAccess/obj/Debug/net8.0/Bulky.DataAccess.AssemblyInfo.cs
+++ BulkyDataAccess/obj/Debug/net8.0/Bulky.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fce7ba0c72ff17ce1d3b69621012b62abf3948c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e51a8f5fccce62bd9717a05e8fe50b2fcbeaa1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
